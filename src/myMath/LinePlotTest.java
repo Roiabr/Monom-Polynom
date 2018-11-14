2bc2bee62174d9acb7c2e8b916c8ac958f8ec7ad
Roi Abramovitch <roiabr15@gmai.com>
2018-11-14 19:00:34 +0200

    fixing the github problems

--- src/myMath/LinePlotTest.java
+++ src/myMath/LinePlotTest.java
@@ -23,26 +23,26 @@ import myMath.Polynom_able;
 
 public class LinePlotTest extends JFrame {
 	public LinePlotTest() {
-		DataTable data=new DataTable(Double.class,Double.class);
-		DataTable data1=new DataTable(Double.class,Double.class);
+		DataTable data=new DataTable(Double.class,Double.class);//this is an object the collect all the points
+		DataTable data1=new DataTable(Double.class,Double.class);//this is an object the collect all the MaxMin points
 		Polynom p1=new Polynom("0.2x^4-1.5x^3+3.0x^2-x^1-5");
 		Polynom_able p2 = p1.copy();
 		p2 = p1.derivative();
 		System.out.println(p2);
-		for (double x=-2.0;x<=6.0;x+=0.25)
+		for (double x=-2.0;x<=6.0;x+=0.25)//this is a for to get all the points in the range
 		{
-			if(p2.f(x)>0 &&p2.f(x+0.2)<0 || p2.f(x)<0 && p2.f(x+0.2)>0) {
-				double i = p2.root(x,x+0.6,0.00000000001);
+			if(p2.f(x)>0 &&p2.f(x+0.2)<0 || p2.f(x)<0 && p2.f(x+0.2)>0) {//if the current point is above the x Axis and the next one is below the x axis
+				double i = p2.root(x,x+0.6,0.00000000001);//finding the extremum
 				data1.add(i,p1.f(i));
 			}
 		
 
-			double y=p1.f(x);
+			double y=p1.f(x);//geting the point of the polynom
 			data.add(x,y);	
 			System.out.println("X:"+x + "," + "Y:" +y);
 		}
 		
-		DataSource filter = new Convolution(data, null, Mode.REPEAT, 1);
+		DataSource filter = new Convolution(data, null, Mode.REPEAT, 1);//to cover the points that arents extremum
 		XYPlot plot = new XYPlot(filter,data1);
 		getContentPane().add(new InteractivePanel(plot));
 		LineRenderer lines = new DefaultLineRenderer2D();
